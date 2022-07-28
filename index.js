c6417a1c20a2c4e6279700bf7eb0c150b4c8f3db
Max <jmlong.dev@gmail.com>
2022-07-28 17:00:16 -0400

    O(1)

--- index.js
+++ index.js
@@ -52,19 +52,19 @@ function funChallenge(input) {
 
 // What is the Big O of the below function? (Hint, you may want to go line by line)
 function anotherFunChallenge(input) {
-  let a = 5;
-  let b = 10;
-  let c = 50;
+  let a = 5; // O(1)
+  let b = 10; // O(1)
+  let c = 50; // O(1)
   for (let i = 0; i < input; i++) {
-    let x = i + 1;
-    let y = i + 2;
-    let z = i + 3;
+    let x = i + 1; // O(1)
+    let y = i + 2; // O(1)
+    let z = i + 3; // O(1)
   }
   for (let j = 0; j < input; j++) {
-    let p = j * 2;
-    let q = j * 2;
+    let p = j * 2; // O(1)
+    let q = j * 2; // O(1)
   }
-  let whoAmI = "I don't know";
+  let whoAmI = "I don't know"; // O(1)
 }
 
 console.log(anotherFunChallenge(10));
