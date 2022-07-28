3bc4aeb0e02b1af2572cc01db2217d7c2a4e7d54
Max <jmlong.dev@gmail.com>
2022-07-28 17:03:28 -0400

    O(1)

--- index.js
+++ index.js
@@ -37,17 +37,18 @@ const logFirstTwoBoxes = (arr) => {
 
 // What is the Big O of the below function? (Hint, you may want to go line by line)
 function funChallenge(input) {
-  let a = 10;
-  a = 50 + 3;
+  let a = 10; // O(1)
+  a = 50 + 3; // O(1)
 
   for (let i = 0; i < input.length; i++) {
-    anotherFunction();
-    let stranger = true;
-    a++;
+    //O(n)
+    anotherFunction(); // O(n)
+    let stranger = true; // O(n)
+    a++; // O(n)
   }
-  return a;
+  return a; // O(1)
 }
-
+// O(3 + 3n)
 // console.log(funChallenge(10)); // 0(n)
 
 // What is the Big O of the below function? (Hint, you may want to go line by line)
@@ -56,15 +57,17 @@ function anotherFunChallenge(input) {
   let b = 10; // O(1)
   let c = 50; // O(1)
   for (let i = 0; i < input; i++) {
-    let x = i + 1; // O(1)
-    let y = i + 2; // O(1)
-    let z = i + 3; // O(1)
+    // 0(n)
+    let x = i + 1; // O(n)
+    let y = i + 2; // O(n)
+    let z = i + 3; // O(n)
   }
   for (let j = 0; j < input; j++) {
-    let p = j * 2; // O(1)
-    let q = j * 2; // O(1)
+    // O(n)
+    let p = j * 2; // O(n)
+    let q = j * 2; // O(n)
   }
   let whoAmI = "I don't know"; // O(1)
 }
-
-console.log(anotherFunChallenge(10));
+// O(4 + 7n)
+// console.log(anotherFunChallenge(10));
